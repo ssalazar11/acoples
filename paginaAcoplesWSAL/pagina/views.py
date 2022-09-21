dd2f41cd44fdcaaff78befc1feb3afb4aa4101d1
Samuel Salazar <ssalazar1@eafit.edu.co>
2022-09-20 21:17:04 -0500

    cosas nuevas

--- paginaAcoplesWSAL/pagina/views.py
+++ paginaAcoplesWSAL/pagina/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .forms import productoForm
+from .models import producto
 # Create your views here.
 
 def index(request):
-    form=productoForm()
+    form=productoForm(request.POST)
+    if form.is_valid():
+        form.save()
     context={'form':form}
     return render(request, 'index.html', context)
+
+ def resultado(request, codigo):
+    objeto=producto.objects.get(codigo=codigo)
+    return render(request)
